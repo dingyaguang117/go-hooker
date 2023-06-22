f10b7f2b81fae96760724213b6e546db07c36392
dingyaguang <dingyaguang@itiger.com>
2023-06-22 10:26:23 +0800

    使用泛型

--- hooker/hooker.go
+++ hooker/hooker.go
@@ -1,40 +1,17 @@
 package hooker
 
-import (
-	"fmt"
-)
-
-// Func 原始函数原型
-type Func func(a, b int) int
-
 // Hook 原型
-type Hook func(Func) Func
-
-// SomeFunc 原始函数定义
-func SomeFunc(a, b int) int {
-	fmt.Printf("%d + %d\n", a, b)
-	return a + b
-}
-
-// Hook1 Hook
-var Hook1 Hook = func(f Func) Func {
-	return func(a, b int) int {
-		fmt.Println("----- before ----")
-		c := f(a, b)
-		fmt.Println("----- after ----")
-		return c
-	}
-}
+type Hook[Func any] func(Func) Func
 
 // Hooker 将 Hook 操作封装起来
-type Hooker struct {
-	origin  Func   // 原始函数
-	wrapped Func   // 包装后的函数
-	hooks   []Hook // 钩子函数列表
+type Hooker[Func any] struct {
+	origin  Func         // 原始函数
+	wrapped Func         // 包装后的函数
+	hooks   []Hook[Func] // 钩子函数列表
 }
 
-func NewHooker(origin Func, hooks ...Hook) *Hooker {
-	h := &Hooker{
+func NewHooker[Func any](origin Func, hooks ...Hook[Func]) *Hooker[Func] {
+	h := &Hooker[Func]{
 		origin: origin,
 		hooks:  hooks,
 	}
@@ -46,6 +23,10 @@ func NewHooker(origin Func, hooks ...Hook) *Hooker {
 	return h
 }
 
-func (h *Hooker) Run(a, b int) int {
-	return h.wrapped(a, b)
+//func (h *Hooker[Func]) Run(a, b int) int {
+//	return h.wrapped(a, b)
+//}
+
+func (h *Hooker[Func]) GetWrappedFunc() Func {
+	return h.wrapped
 }
--- hooker/hooker_test.go
+++ hooker/hooker_test.go
@@ -1,8 +1,28 @@
 package hooker
 
 import (
+	"fmt"
 	"testing"
 )
+
+// Func 原始函数原型
+type Func func(a, b int) int
+
+// SomeFunc 原始函数定义
+func SomeFunc(a, b int) int {
+	fmt.Printf("%d + %d\n", a, b)
+	return a + b
+}
+
+// Hook1 Hook
+var Hook1 Hook[Func] = func(f Func) Func {
+	return func(a, b int) int {
+		fmt.Println("----- before ----")
+		c := f(a, b)
+		fmt.Println("----- after ----")
+		return c
+	}
+}
 
 func TestHookFunc(t *testing.T) {
 	var func1 = Hook1(SomeFunc)
@@ -10,6 +30,6 @@ func TestHookFunc(t *testing.T) {
 }
 
 func TestHookFunc2(t *testing.T) {
-	hooker := NewHooker(SomeFunc, Hook1, Hook1)
-	hooker.Run(1, 2)
+	hooker := NewHooker[Func](SomeFunc, Hook1, Hook1)
+	hooker.GetWrappedFunc()(1, 2)
 }
